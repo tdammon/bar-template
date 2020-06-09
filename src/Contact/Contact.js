7207b4cb7959b2ed3e9b7cf891fb39a6c0d1c3d0
Trevor Dammon <trevordammon.evolvmobile@gmail.com>
2020-06-09 08:40:47 -0500

    .

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import style from "./Contact.module.css"
 
 class DesignView extends Component {
 
   render() {
     return (
-      <div >
+      <div className={style.app}>
       <div style={{width: "100%", height: "100%", backgroundColor:"white", padding: "1rem", fontSize:"2.5rem"}}>Under Construction</div>
 
       </div>
