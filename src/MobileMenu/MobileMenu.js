f58cf6a5aeb500f57c4c1579270418d433472611
Trevor Dammon <trevordammon.evolvmobile@gmail.com>
2020-07-12 10:20:41 -0500

    updates

--- src/MobileMenu/MobileMenu.js
+++ src/MobileMenu/MobileMenu.js
@@ -3,6 +3,7 @@ import style from "./MobileMenu.module.css";
 import CallIcon from '@material-ui/icons/Call';
 import companyLogo from "../CompanyImages/FrenchPress.png"
 import homePicture from "../CompanyImages/cheese-and-bread.jpg";
+import hamburgerMenu from "../CompanyImages/hamburgerMenu.png";
 import BreakfastMenu from "./BreakfastMenu"
 import LunchMenu from "./LunchMenu"
 import DinnerMenu from "./DinnerMenu"
@@ -29,13 +30,30 @@ const MobileMenu = () => {
     }
   }
 
+  const loadImage = () => {
+    switch(menu) {
+        case 'breakfast':
+        return(<div className={style.menuHeader}><img className={style.menuImage} src={homePicture} alt="Bread and Cheese"/><div className={style.menuBanner}>BREAKFAST</div></div>)
+        break;
+        case 'lunch':
+        return(<div><img className={style.menuImage} src={homePicture} alt="Bread and Cheese"/><div>Lunch</div></div>)
+        break;
+        case 'dinner':
+        return(<div><img className={style.menuImage} src={homePicture} alt="Bread and Cheese"/><div>Dinner</div></div>)
+        break;
+        case 'dessert':
+        return(<div><img className={style.menuImage} src={homePicture} alt="Bread and Cheese"/><div>Dessert</div></div>)
+        break;
+    }
+  }
+
   const changeMenu = list => {
       setMenu(list)
   }
 
     return (
       <div className={style.app}>
-        <div className={style.mobileHeader}>
+        {/* <div className={style.mobileHeader}>
           <div className={style.logoBox}>
             <img className={style.logoPicture} src={companyLogo} alt="French Press Logo" />
           </div>
@@ -45,13 +63,24 @@ const MobileMenu = () => {
               <CallIcon style={{fontSize: "3rem"}}/>
             </a>
           </div>
+        </div> */}
+        <div className={style.mobileHeader}>
+        <div style={{fontFamily: "'Lucida Sans Unicode', Geneva, Verdana, sans-serif", margin: "1rem", height: "3rem", width: "3rem", color: "white", backgroundColor: "rgba(0, 0, 0, 0.9)", alignItems: "center", display: "flex", justifyContent:"center"}}>
+          Logo
+        </div>
+        <div style={{margin: "1.25rem", height: "2.5rem", width: "2.5rem", textAlign: "center"}}>
+        <img className={style.menuImage} src={hamburgerMenu} alt="hamburger menu icon"/>
+        </div>
         </div>
         <div className={style.mainContent}>
             <div className={style.menuNav}>
-                <div className={menu ==='breakfast' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('breakfast')}>Breakfast</div>
+            <div>
+              {loadImage()}
+            </div>
+                {/* <div className={menu ==='breakfast' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('breakfast')}>Breakfast</div>
                 <div className={menu ==='lunch' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('lunch')}>Lunch</div>
                 <div className={menu ==='dinner' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('dinner')}>Dinner</div>
-                <div className={menu ==='dessert' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('dessert')}>Dessert</div>
+                <div className={menu ==='dessert' ? style.menuNavButtonActive : style.menuNavButton} onClick={()=>changeMenu('dessert')}>Dessert</div> */}
             </div>
                 {loadMenu()}
         </div>
