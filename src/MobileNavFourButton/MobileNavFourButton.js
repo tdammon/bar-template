9a05d2cbf05ea4fc7f13053ac9cc86381cfc03ae
Trevor Dammon <trevor@Trevors-MacBook-Pro.local>
2020-10-08 21:13:20 -0500

    .

--- src/MobileNavFourButton/MobileNavFourButton.js
+++ src/MobileNavFourButton/MobileNavFourButton.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import style from "./MobileNavFourButton.module.css"
-import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import CallIcon from '@material-ui/icons/Call';
-import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
-import CreateIcon from '@material-ui/icons/Create';
-import RestaurantMenuOutlinedIcon from '@material-ui/icons/RestaurantMenuOutlined';
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import style from "./MobileNavFourButton.module.css";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import CallIcon from "@material-ui/icons/Call";
+import PhoneOutlinedIcon from "@material-ui/icons/PhoneOutlined";
+import CreateIcon from "@material-ui/icons/Create";
+import RestaurantMenuOutlinedIcon from "@material-ui/icons/RestaurantMenuOutlined";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 
+const MobileNavFourButton = () => {
+  const history = useHistory();
 
-const MobileNavFourButton=()=> {
-    const history =useHistory()
-
-    return (
-      <div className={style.NavBar}>
-        <div className={style.NavButton} onClick={()=>history.push("/menu")}>
-        <HomeOutlinedIcon style={{fontSize: "1.25rem"}}/><div className={style.buttonText}>Menu</div>
-        </div>
-        <div className={style.NavButton} onClick={()=>history.push("/specials")}>
-        <RestaurantMenuOutlinedIcon style={{fontSize: "1.25rem"}}/><div className={style.buttonText}>Cater</div>
-        </div>
-        <div className={style.NavButton} onClick={()=>history.push("/catering")}>
-        <StarBorderOutlinedIcon style={{fontSize: "1.25rem"}}/><div className={style.buttonText}>Specials</div>
-        </div>
-        <a className={style.NavButton} href="tel:+900300400" >
-        <PhoneOutlinedIcon style={{fontSize: "1.25rem"}}/>
-        <div className={style.buttonText} >Call Now</div>
-        </a>
+  return (
+    <div className={style.NavBar}>
+      <div className={style.NavButton} onClick={() => history.push("/menu")}>
+        <HomeOutlinedIcon style={{ fontSize: "1.25rem" }} />
+        <div className={style.buttonText}>Drinks</div>
       </div>
-    );
-}
+      <div
+        className={style.NavButton}
+        onClick={() => history.push("/specials")}
+      >
+        <RestaurantMenuOutlinedIcon style={{ fontSize: "1.25rem" }} />
+        <div className={style.buttonText}>Events</div>
+      </div>
+      <div
+        className={style.NavButton}
+        onClick={() => history.push("/catering")}
+      >
+        <StarBorderOutlinedIcon style={{ fontSize: "1.25rem" }} />
+        <div className={style.buttonText}>Specials</div>
+      </div>
+      <a className={style.NavButton} href="tel:+900300400">
+        <PhoneOutlinedIcon style={{ fontSize: "1.25rem" }} />
+        <div className={style.buttonText}>Call Now</div>
+      </a>
+    </div>
+  );
+};
 
-export default (MobileNavFourButton);
+export default MobileNavFourButton;
