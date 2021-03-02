2e99f490a3097cfd12d1776e9cb138caae39d9ac
Trevor Dammon <trevor@Trevors-MacBook-Pro.local>
2021-03-02 11:16:48 -0600

    .

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,15 +5,7 @@ class DesignView extends Component {
   render() {
     return (
       <div className={style.app}>
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "white",
-            padding: "1rem",
-            fontSize: "2.5rem",
-          }}
-        >
+        <div>
           <div className={style.section}>
             <iframe
               className={style.iframe}
